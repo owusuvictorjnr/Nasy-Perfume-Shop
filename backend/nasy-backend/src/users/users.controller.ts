47c0fa89e1e5b458b82ed0e6bca5cea44dba0587
owusuvictorjnr <owusuvictor80@gmail.com>
2025-12-03 07:30:58 +0000

    update controller

--- backend/nasy-backend/src/users/users.controller.ts
+++ backend/nasy-backend/src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import * as admin from 'firebase-admin';
 import { FirebaseAuthGuard } from 'src/auth/firebase-auth.guard';
+// import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('users')
 export class UsersController {
@@ -26,3 +27,35 @@ export class UsersController {
     };
   }
 }
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
+// import { User } from 'src/users/user.entity';
+// import { UsersModule } from 'src/users/users.module';
+// import { AuthModule } from 'src/auth/auth.module';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: '.env',
+//     }),
+
+//     // PostgreSQL connection
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: process.env.DB_HOST ?? 'localhost',
+//       port: Number(process.env.DB_PORT ?? 5432),
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASS,
+//       database: process.env.DB_NAME,
+//       entities: [User],
+//       synchronize: true, // turn OFF in production
+//     }),
+
+//     UsersModule,
+//     AuthModule,
+//   ],
+// })
+// export class AppModule {}
