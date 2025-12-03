f4efb58407618e2eab3850405e06f965121be4a2
owusuvictorjnr <owusuvictor80@gmail.com>
2025-12-03 07:31:25 +0000

    out user controller

--- backend/nasy-backend/src/users/users.module.ts
+++ backend/nasy-backend/src/users/users.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [UsersController],
+  // controllers: [UsersController],
 })
 export class UsersModule {}
